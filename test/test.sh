99efe8f0c3e4554eae24af7b81f1a8f9f0941a36
Loren Segal <lsegal@soen.ca>
2016-09-04 23:22:59 -0700

    Add test script

---
+++ test/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export GODEBUG="cgocheck=0"
+
+BCMD="go run cmd/gorbgen/main.go -build"
+TCMD="ruby"
+
+set -e
+shopt -s globstar nullglob dotglob
+for d in test/*/**; do
+  if [ -d $d ]; then
+    files=($d/*.go)
+    if [ ${#files[@]} -gt 0 ]; then
+      echo $BCMD $d
+      $BCMD $d
+
+      testfiles=(ext/$d/test_*.rb)
+      if [ ${#testfiles[@]} -gt 0 ]; then
+        echo $TCMD $testfiles
+        $TCMD $testfiles
+      fi
+    fi
+  fi
+done
